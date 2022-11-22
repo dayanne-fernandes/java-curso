693002d99ea3b0c22342f7ecc204ce4ff9b011c0
Charles Guedes <charlesgcf@gmail.com>
2022-11-22 12:23:44 -0300

    aula 00 ate 10

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+    // isso é um comentario de uma linha
+    /*
+    isso é um comentario
+     de multiplas
+     linhas
+     */
+        public static void main(String[] args) {
+            System.out.println("KA-ME-KA-ME-KA");
+        }
+    }
+
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02tiposprimitivos.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02tiposprimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age=
+    }
+}
