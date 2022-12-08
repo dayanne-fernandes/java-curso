e32bf9868ad0876e1ab6de4f3ada6b2c41a98135
Dayanne Fernandes <dayannefernandes194@gmail.com>
2022-12-07 22:02:06 -0300

    aula05continuacao02

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais01 {
+    public static void main(String[] args){
+        int idade = 15;
+        boolean isAutorizadocomprarBebida = idade >=18;
+        if (isAutorizadocomprarBebida != false) {
+            boolean isAutorizadoComprarBebidaAlcolica = idade >=18;
+            System.out.println("Autorizado a comprar bebida alcólica");
+
+        }else{
+            System.out.println("Não Autorizado a comprar bebida alcólica");
+
+        }
+        if(!isAutorizadocomprarBebida);{
+            System.out.println("Não Autorizado a comprar bebida alcólica");
+        }
+
+        boolean c = false;
+                if(c = true){
+                    System.out.println("Dentro de algo que nunca deve ser feito");
+                }
+            System.out.println("Fora do if");
+
+
+    }
+}
