f1c4a72bc6d4b3d03fd67308c6da0ba1204ae65d
Dayanne Fernandes <dayannefernandes194@gmail.com>
2023-02-03 10:25:41 -0300

    impressoraestudante

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -37,4 +37,11 @@ public class Calculadora {
         System.out.println(num1/num2);
 
     }
+        public void alteraDoisNumeros(int numero1, int numero2){
+        numero1 = 99;
+        numero2 = 33;
+            System.out.println("Dentro do alteraDoisNumeros");
+            System.out.println("num1 "+numero1);
+            System.out.println("num2 "+numero2);
+    }
 }
