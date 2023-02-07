a31fe3ae0f920d47a8f3c092a95db877316e4841
Dayanne Fernandes <dayannefernandes194@gmail.com>
2023-02-07 11:04:11 -0300

    continuaçao test

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -44,4 +44,23 @@ public class Calculadora {
             System.out.println("num1 "+numero1);
             System.out.println("num2 "+numero2);
     }
+
+    public void somaArray(int[] numeros){
+        int soma = 0;
+        for(int num: numeros){
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+    public void somaVarArgs(int... numeros){
+        int soma = 0;
+        for(int num: numeros){
+            soma += num;
+    }
+
+        System.out.println(soma);
+
+    }
+
 }
+
