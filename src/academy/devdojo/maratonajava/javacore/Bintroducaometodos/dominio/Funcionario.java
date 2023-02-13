d9fd2a0930def89115c0640b70fb74a816b65822
Dayanne Fernandes <dayannefernandes194@gmail.com>
2023-02-13 16:43:08 -0300

    dominiopessoa

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Funcionario {
+    public String nome;
+    public int idade;
+    public double[] salarios;
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        for (double salario : salarios) {
+            System.out.print(salario + " ");
+        }
+        imprimeMediaSalario();
+    }
+
+    public void imprimeMediaSalario() {
+        double media = 0;
+        for (double salario : salarios) {
+            media += salario;
+
+        }
+        media /= salarios.length;
+        System.out.println("\nMédia salarial " + media);
+
+    }
+}
